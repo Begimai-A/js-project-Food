a3c5d0d4540a1016fa8a161d4611c0dad72e563e
Begimai <begimai.akulova@gmail.com>
2021-03-02 03:27:44 +0300

    add new function with the help of the reg.expression and method replace to modify slider

--- js/script.js
+++ js/script.js
@@ -334,7 +334,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     const slides = document.querySelectorAll('.offer__slide'),
-        slider=document.querySelector('.offer__slider'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -356,12 +356,12 @@ document.addEventListener('DOMContentLoaded', () => {
     slides.forEach(slide => {
         slide.style.width = width;
     });
- 
-    slider.style.position='relative';
-    const indicators=document.createElement('ol'),
-    dots=[];
+
+    slider.style.position = 'relative';
+    const indicators = document.createElement('ol'),
+        dots = [];
     indicators.classList.add('carousel-indicators');
-    indicators.style.cssText=`
+    indicators.style.cssText = `
             position: absolute;
             right: 0;
             bottom: 0;
@@ -375,10 +375,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     slider.append(indicators);
 
-    for (let i=0; i<slides.length; i++) {
-        const dot=document.createElement('li');
-        dot.setAttribute('data-slide-to', i+1)
-        dot.style.cssText=`
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1)
+        dot.style.cssText = `
         box-sizing: content-box;
         flex: 0 1 auto;
         width: 30px;
@@ -393,21 +393,25 @@ document.addEventListener('DOMContentLoaded', () => {
         opacity: .5;
         transition: opacity .6s ease;
         `;
-        
-        if (i==0){
-            dot.style.opacity=1;
+
+        if (i == 0) {
+            dot.style.opacity = 1;
         }
 
         indicators.append(dot);
         dots.push(dot);
     }
 
-   
+    function deleteNotDigits(str){
+        return +str.replace(/\D/g, '');
+    }
+
+
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) { // при помощи метода replace () все нечисла заменятся пустой строкой ''
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`; // translateX ззначит по горизонтали; знак "-" означает налево
 
@@ -422,15 +426,15 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent = slideIndex;
         }
-            dots.forEach(dot=>dot.style.opacity='.5');
-            dots[slideIndex-1].style.opacity=1;
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
     });
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
         slidesField.style.transform = `translateX(-${offset}px)`; // translateX ззначит по горизонтали; знак "-" означает налево
 
@@ -447,15 +451,15 @@ document.addEventListener('DOMContentLoaded', () => {
             current.textContent = slideIndex;
         }
 
-            dots.forEach(dot=>dot.style.opacity='.5');
-            dots[slideIndex-1].style.opacity=1;
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
     });
 
-    dots.forEach(dot=>{
-        dot.addEventListener('click', (e)=>{
-            const slideTo=e.target.getAttribute('data-slide-to');
-            slideIndex=slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+            slideIndex = slideTo;
+            offset = deleteNotDigits(width) * (slideTo - 1);
             slidesField.style.transform = `translateX(-${offset}px)`;
 
             if (slides.length < 10) {
@@ -463,9 +467,9 @@ document.addEventListener('DOMContentLoaded', () => {
             } else {
                 current.textContent = slideIndex;
             }
-    
-                dots.forEach(dot=>dot.style.opacity='.5');
-                dots[slideIndex-1].style.opacity=1;
+
+            dots.forEach(dot => dot.style.opacity = '.5');
+            dots[slideIndex - 1].style.opacity = 1;
         });
     });
 
